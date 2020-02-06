5ed6c0e9d3351601fc356cc4c42015ccfa0d08e7
moazzam2017 <moazzamwaheed@gmail.com>
2020-02-06 20:51:09 +0500

    Commit Angular

--- CarParkArcGisApi/WebProject/ClientApp/e2e/src/app.e2e-spec.ts
+++ CarParkArcGisApi/WebProject/ClientApp/e2e/src/app.e2e-spec.ts
@@ -1,14 +1,31 @@
-import { AppPage } from './app.po';
+import { AppPage } from "./app.po";
+import { browser, logging } from "protractor";
 
-describe('App', () => {
+describe("workspace-project App", () => {
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it("should include a link to the JS API", () => {
     page.navigateTo();
-    expect(page.getMainHeading()).toEqual('Hello, world!');
+    browser.waitForAngularEnabled(false);
+    expect(page.getApiUrl()).toEqual(
+      "https://developers.arcgis.com/javascript/"
+    );
+  });
+
+  afterEach(async () => {
+    // Assert that there are no errors emitted from the browser
+    const logs = await browser
+      .manage()
+      .logs()
+      .get(logging.Type.BROWSER);
+    expect(logs).not.toContain(
+      jasmine.objectContaining({
+        level: logging.Level.SEVERE
+      } as logging.Entry)
+    );
   });
 });
--- CarParkArcGisApi/WebProject/ClientApp/src/app/app.component.ts
+++ CarParkArcGisApi/WebProject/ClientApp/src/app/app.component.ts
@@ -2,8 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'app';
+  // Set our map properties
+  mapCenter = [-122.4194, 37.7749];
+  basemapType = 'satellite';
+  mapZoomLevel = 12;
+
+  // See app.component.html
+  mapLoadedEvent(status: boolean) {
+    console.log('The map loaded: ' + status);
+  }
 }
+
