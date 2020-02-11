22b9ce2723c013e92e8b9572def1296f8f1ad8b6
moazzam2017 <moazzamwaheed@gmail.com>
2020-02-11 22:07:05 +0500

    committed

--- CarParkArcGisApi/CarParkArcGisApi/FindRouteApi.py
+++ CarParkArcGisApi/CarParkArcGisApi/FindRouteApi.py
@@ -7,7 +7,7 @@ gis = GIS('https://www.arcgis.com', user_name, password)
 
 address_1 = input()
 
-search_result = gis.content.search(address_1, "feature collection", max_items=1)
+search_result = gis.content.search("title:"+address_1, item_type = "Web Map")
 
 if len(search_result) == 0:
     print("list is empty")
@@ -18,12 +18,4 @@ else:
     map1 = gis.map(address_1)
     map1
 
-    map1.add_layer(search_item)
-
-    search_fc = search_item.layers[3]
-
-    from arcgis.features import find_locations
-
-    trace1 = find_locations.trace_downstream(search_fc)
-
-    map1.add_layer(trace1)
+    
