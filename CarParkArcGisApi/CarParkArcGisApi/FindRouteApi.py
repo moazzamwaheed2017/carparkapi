3a8369faf71e83d7195ee3d7b8732ed14d84cfff
moazzam2017 <moazzamwaheed@gmail.com>
2020-02-11 20:10:38 +0500

    committed

--- CarParkArcGisApi/CarParkArcGisApi/FindRouteApi.py
+++ CarParkArcGisApi/CarParkArcGisApi/FindRouteApi.py
@@ -1,22 +1,29 @@
 
 from arcgis.gis import GIS
-gis = GIS('https://www.arcgis.com', "arcgis_python", "P@ssword123")
+
+user_name = 'arcgis_python'
+password = 'P@ssword123'
+gis = GIS('https://www.arcgis.com', user_name, password)
 
 address_1 = input()
 
 search_result = gis.content.search(address_1, "feature collection", max_items=1)
-search_item = search_result[0]
-search_item
 
-map1 = gis.map("Big bear, California")
-map1
+if len(search_result) == 0:
+    print("list is empty")
+else:    
+    search_item = search_result[0]
+    search_item
 
-map1.add_layer(search_item)
+    map1 = gis.map(address_1)
+    map1
 
-search_fc = search_item.layers[3]
+    map1.add_layer(search_item)
 
-from arcgis.features import find_locations
+    search_fc = search_item.layers[3]
 
-trace1 = find_locations.trace_downstream(search_fc)
+    from arcgis.features import find_locations
 
-map1.add_layer(trace1)
+    trace1 = find_locations.trace_downstream(search_fc)
+
+    map1.add_layer(trace1)
--- CarParkArcGisApi/CarparkingApiUnitTest/FindRouteApiTest.py
+++ CarParkArcGisApi/CarparkingApiUnitTest/FindRouteApiTest.py
@@ -1,19 +1,13 @@
+
 import unittest
+from routeapi import FindRouteApi
 
 import sys
 
 class Test_FindRouteApiTest(unittest.TestCase):
-    def setUp(self):
-        self.distance = GetTimeAndDistance()
+    
+    def test_simple_should_return_the_distance(self):
+		self.assertEqual()
 
-    def test_A(self):
-        self.fail("Not implemented")
-
-if __name__ == '__main__':
-    import xmlrunner
-
-    unittest.main(
-        testRunner=xmlrunner.XMLTestRunner(output='test-reports'),
-        failfast=False,
-        buffer=False,
-        catchbreak=False)
+if __name__ == '__main__': 
+    unittest.main() 
